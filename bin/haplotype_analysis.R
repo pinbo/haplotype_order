5927d48e4eba912c543f23b58e75cba10bbd7784
Junli Zhang <zhjl86@gmail.com>
2019-09-05 17:39:28 -0700

    fix a bug for hclust in R and add a maximum missing value filter for lines

--- bin/haplotype_analysis.R
+++ bin/haplotype_analysis.R
@@ -8,6 +8,7 @@ if (length(args)==0) {
 
 snpfile = args[1]
 metadata = args[2]
+maxmiss = as.numeric(args[3]) # maximum missing values to keep a line
 
 dd= read.delim(snpfile, check.names = F, as.is = T, na.strings = "N")
 md = read.delim(metadata)
@@ -34,19 +35,39 @@ dimnames(snp) = list(colnames(dd)[6:nc], snpname)
 
 dd3 = dd2 - 1
 
+# to see the maximum missing values I can keep.
+for (i in rev(seq(0.1, 0.9, 0.05))){
+  dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*i, ]
+  snp.dist <- dist(dd4)
+  if (sum(is.na(snp.dist)) == 0) break
+}
+i # the final missing proportion
+maxmiss = ifelse(maxmiss < i, maxmiss, i)
+dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*maxmiss, ]
+dim(dd4)
+
 ##
-snp.dist <- dist(dd3)
+snp.dist <- dist(dd4)
 fit <- hclust(snp.dist,method="ward.D2")
 str(fit)
 pdf(file="dendrogram_of_lines.pdf", width = 10)
 plot(fit, hang=-1, labels=FALSE)
 # rect.hclust(fit, h = 10, border = "red")
 dev.off()
-
-snp2 = snp[fit$order,]
-md2 = md[rownames(snp2),]
+ordered_line_names = fit$labels[fit$order]
+snp2 = snp[ordered_line_names,]
+md2 = md[ordered_line_names,]
+dd5 = dd4[ordered_line_names,]
 all(rownames(snp2) == rownames(md2))
 outdata = cbind(md2, snp2)
-#outdata[1:4,1:30]
-write.table(outdata, "haplotype_ordered_genotable.txt", row.names = F, sep="\t", quote=F)
+outdata2 = cbind(md2, dd5)
 
+outdata3 = cbind(md[rownames(snp),], snp)
+#outdata[1:4,1:30]
+
+### write all output data
+write.table(outdata, "haplotype_ordered_genotable.txt", row.names = F, sep="\t", quote=F, na = "-")
+
+write.table(outdata2, "haplotype_ordered_genotable_numbers.txt", row.names = F, sep="\t", quote=F, na = "-")
+
+write.table(outdata3, "raw_extracted_genotable.txt", row.names = F, sep="\t", quote=F, na = "-")
