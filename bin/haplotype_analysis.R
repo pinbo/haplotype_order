d7d6ac2d4f71c7d98debd9c67afaa29937dfc1db
Junli Zhang <zhjl86@gmail.com>
2019-09-08 12:13:22 -0700

    add the R script with filtering of missing marker data

--- bin/haplotype_analysis.R
+++ bin/haplotype_analysis.R
@@ -2,13 +2,14 @@
 
 args = commandArgs(trailingOnly=TRUE)
 
-if (length(args)==0) {
-  stop("Please give the marker data and metadata file\n")
+if (length(args) < 3) {
+  stop("Please give the marker data, metadata file, maximum missing data for lines, and maximum missing data for markers\n")
 }
 
 snpfile = args[1]
 metadata = args[2]
-maxmiss = as.numeric(args[3]) # maximum missing values to keep a line
+maxmissl = as.numeric(args[3]) # maximum missing values to keep a line
+maxmissm = as.numeric(args[4]) # maximum missing values to keep a line
 
 dd= read.delim(snpfile, check.names = F, as.is = T, na.strings = "N")
 md = read.delim(metadata)
@@ -28,12 +29,14 @@ dd2 = sapply(1:nr, function(x) {
   tt2 = as.numeric(tt)
 })
 
-#dd2[1:20,1:6]
 
 dimnames(dd2) = list(colnames(dd)[6:nc], snpname)
 dimnames(snp) = list(colnames(dd)[6:nc], snpname)
 
-dd3 = dd2 - 1
+dd2.1 = dd2 - 1
+
+# remove markers with too many missing values
+dd2.2 = dd2.1[ , colSums(is.na(dd2.1)) < nrow(dd2.1)*maxmissm ]
 
 # to see the maximum missing values I can keep.
 for (i in rev(seq(0.1, 0.9, 0.05))){
@@ -42,9 +45,18 @@ for (i in rev(seq(0.1, 0.9, 0.05))){
   if (sum(is.na(snp.dist)) == 0) break
 }
 i # the final missing proportion
-maxmiss = ifelse(maxmiss < i, maxmiss, i)
-dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*maxmiss, ]
+maxmissl = ifelse(maxmissl < i, maxmissl, i)
+dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*maxmissl, ]
 dim(dd4)
+
+# remove monomorphic markers
+tokeep = apply(dd4, 2, function(x){
+  n1 = sum(x == 1, na.rm = T)
+  n2 = sum(x == 2, na.rm = T)
+  min(n1, n2) > 1 # now keep markers with at least 2 minor alleles
+})
+
+dd4 = dd4[,tokeep]
 
 ##
 snp.dist <- dist(dd4)
@@ -59,15 +71,12 @@ snp2 = snp[ordered_line_names,]
 md2 = md[ordered_line_names,]
 dd5 = dd4[ordered_line_names,]
 all(rownames(snp2) == rownames(md2))
-outdata = cbind(md2, snp2)
-outdata2 = cbind(md2, dd5)
-
-outdata3 = cbind(md[rownames(snp),], snp)
+outdata = cbind(md2, snp2) # with A, T, G, C alleles
+outdata2 = cbind(md2, dd5) # with numbers: 0 is ref, 2 is alt, 1 is het.
+outdata3 = cbind(md[rownames(snp),], snp) # non-filtered raw data
 #outdata[1:4,1:30]
 
 ### write all output data
 write.table(outdata, "haplotype_ordered_genotable.txt", row.names = F, sep="\t", quote=F, na = "-")
-
 write.table(outdata2, "haplotype_ordered_genotable_numbers.txt", row.names = F, sep="\t", quote=F, na = "-")
-
 write.table(outdata3, "raw_extracted_genotable.txt", row.names = F, sep="\t", quote=F, na = "-")
