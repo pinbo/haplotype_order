f6b16a72a0d87bc8ca5909ae66d3bf18edd6a21f
Junli Zhang <zhjl86@gmail.com>
2020-08-29 06:32:17 -0700

    add scripts for given vcf subset

---
+++ bin/haplotype_analysis_no_metadata.R
@@ -0,0 +1,91 @@
+#!/usr/bin/env Rscript
+
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args) < 3) {
+  stop("Please give the marker data, maximum missing data for lines, and maximum missing data for markers\n")
+}
+
+snpfile = args[1]
+maxmissl = as.numeric(args[2]) # maximum missing values to keep a line
+maxmissm = as.numeric(args[3]) # maximum missing values to keep a marker
+
+dd= read.delim(snpfile, check.names = F, as.is = T, na.strings = "N")
+dim(dd)
+
+nc = ncol(dd)
+nr = nrow(dd)
+
+snp = t(dd[,6:nc])
+#snp[1:5,1:5]
+snpname = apply( dd[ , c(1,2,4,5) ] , 1 , paste , collapse = "-" )
+snpname
+dd2 = sapply(1:nr, function(x) {
+  tt = factor(dd[x, 6:nc], levels = c(dd[x,4], "H", dd[x,5]))
+  tt2 = as.numeric(tt)
+})
+
+
+dimnames(dd2) = list(colnames(dd)[6:nc], snpname)
+dimnames(snp) = list(colnames(dd)[6:nc], snpname)
+
+dd2[1:4,1:4]
+snp[1:4,1:4]
+
+dd2.1 = dd2 - 1
+
+# remove markers with too many missing values
+dd3 = dd2.1[ , colSums(is.na(dd2.1)) < nrow(dd2.1)*maxmissm ]
+
+# to see the maximum missing values I can keep.
+for (i in rev(seq(0.1, 0.9, 0.05))){
+  dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*i, ]
+  snp.dist <- dist(dd4)
+  if (sum(is.na(snp.dist)) == 0) break
+}
+i # the final missing proportion
+maxmissl = ifelse(maxmissl < i, maxmissl, i)
+dd4 = dd3[rowSums(is.na(dd3)) < ncol(dd3)*maxmissl, ]
+dim(dd4)
+
+# remove monomorphic markers
+tokeep = apply(dd4, 2, function(x){
+  n1 = sum(x == 0, na.rm = T)
+  n2 = sum(x == 2, na.rm = T)
+  min(n1, n2) > 0 # now keep markers with at least 1 minor alleles
+})
+sum(tokeep)
+
+dd4 = dd4[,tokeep]
+dim(dd4)
+
+
+##
+snp.dist <- dist(dd4)
+fit <- hclust(snp.dist,method="ward.D2")
+#str(fit)
+pdf(file="dendrogram_of_lines.pdf", width = 14)
+plot(fit, hang=-1, labels=FALSE)
+rect.hclust(fit, h = 5, border = "blue")
+dev.off()
+
+# get big group number
+#maxheight = max(fit$height)
+ng = cutree(fit, h = 5) # number of group, use 5 as cut threshold
+table(ng)
+
+ordered_line_names = fit$labels[fit$order]
+ng = ng[ordered_line_names]
+dd5 = dd4[ordered_line_names,]
+snp2 = snp[ordered_line_names, colnames(dd4)]
+snp3 = t(snp2)
+cn = colnames(snp2)
+snp.infor = data.frame(do.call('rbind', strsplit(cn,'-',fixed=TRUE)))
+colnames(snp.infor) = c("Chrom", "Pos", "Ref", "Alt")
+snp4 = cbind(snp.infor, snp3)
+outdata = rbind(group=c("Group", rep("", 3), ng), snp4) # with A, T, G, C alleles
+
+
+### write all output data
+write.table(outdata, "haplotype_ordered_genotable.txt", row.names = F, sep="\t", quote=F, na = "-")
+
---
+++ run_haplotype_given_vcf.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "The script you are running has basename `basename "$0"`, dirname `dirname "$0"`"
+echo "The present working directory is `pwd`"
+
+
+vcf_file=$1 # vcf
+#meta_data=$5 # metadata for lines to merge to SNP table
+maxmissl=$2
+maxmissm=$3
+
+script_path="`dirname "$0"`/bin" # scripts folder
+echo $script_path
+
+#step 2: convert from GT to SNP alleles
+cmd2="$script_path/convert_vcf_calls_to_SNP.py $vcf_file out.txt"
+echo "Step 2: Convert vcf to SNP table: $cmd2"
+eval $cmd2
+# Step 3: sort lines by similarity for haplotype analysis
+cmd3="$script_path/haplotype_analysis_no_metadata.R out.txt $maxmissl $maxmissm"
+echo "Step 3: Get the sorted SNP table for haplotype analysis: $cmd3"
+eval $cmd3
+
+echo "Finish successfully! Output data is 'haplotype_ordered_genotable.txt' "
