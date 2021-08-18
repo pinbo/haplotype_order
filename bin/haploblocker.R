76c71bd99c3367a896a3839932d49e5124ff7665
Junli Zhang <zhjl86@gmail.com>
2021-08-17 18:34:33 -0700

    add haploblocker script

---
+++ bin/haploblocker.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+# download the packages here
+# https://github.com/tpook92/HaploBlocker
+# install.packages("~/Downloads/RandomFieldsUtils_0.6.6.tar.gz",type="source", repo=NULL)
+# install.packages("~/Downloads/HaploBlocker_1.6.06.tar.gz",type="source", repo=NULL)
+
+library(HaploBlocker)
+hap1b = read.delim("haplotype_ordered_genotable.txt", na.strings = "-")
+hap1b = hap1b[-nrow(hap1b),]
+dim(hap1b)
+blocklist = block_calculation(dhm=hap1b[,-c(1:4)],bp_map=hap1b[,2],min_majorblock=100,window_size = 5)#,prefilter=T,overlap_remove=T)
+# blocklist = block_calculation(dhm="hap1B-filtered-lines.vcf")#,
+                                 # window_size = 5,
+                                 # prefilter=T
+                                 # )
+pdf(file = "haplo-block-graphs.pdf")
+plot_block(blocklist)
+plot_block(blocklist,type = "bp",xlim=range(hap1b[,2]))
+blocklist_plot(blocklist)
+blocklist_plot_xsize(blocklist)
+dev.off()
+
+# start and end of each block
+blockinfo = blocklist_startend(blocklist)
+# Calculate a block-dataset according to the block library 
+newdata = block_matrix_construction(blocklist)
+newdata2 = cbind(Block=rownames(newdata), blockinfo, newdata)
+write.table(newdata2, "haplo_block_matrix.txt", sep="\t",row.names = F, quote = F)
+
+
+
+
